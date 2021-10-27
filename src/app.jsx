96c6cfcbeb98bd823fef885f51a6fc1ebbc6f227
SeYoung <wnl383@naver.com>
2021-10-27 18:05:10 +0900

    show video list titles

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './Components/video_list/video_list';
 
 function App() {
-  return 
+  const [videos, setVideos] = useState([]);
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',  
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBsDnlq13z2o_dUh2XuG7dpP4IIufr-ipk", requestOptions)
+      .then(response => response.json())  //response를 텍스트로 변환
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return (
+    <React.Fragment>
+      <VideoList videos={videos}/>
+    </React.Fragment>
+  );
 }
 
 export default App;
