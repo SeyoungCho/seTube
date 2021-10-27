4160b8ba12fc1868c7c4f8dc1ecdedb614f82562
SeYoung <wnl383@naver.com>
2021-10-28 01:19:26 +0900

    Search implementation

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './app.css';
+import SearchAppBar from './Components/mui_components/search_app_bar';
 import VideoList from './Components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
   useEffect(()=>{
     const requestOptions = {
       method: 'GET',  
@@ -15,9 +18,28 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
+  
+  const search = ()=>{ //입력된 query를 가지고 search list get요청하는 함수
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchInput}&key=AIzaSyBsDnlq13z2o_dUh2XuG7dpP4IIufr-ipk`, requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  };
+
   return (
     <React.Fragment>
+      <SearchAppBar 
+        searchInput={searchInput} 
+        setSearchInput={setSearchInput}
+        onSearch={search}
+      />
       <VideoList videos={videos}/>
+      {console.log(searchInput)}
     </React.Fragment>
   );
 }
